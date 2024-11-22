e0112db8a1eb128a3509d8fffb55e9268143056f
Vishva Balar <vishubalar29@gmail.com>
2024-11-22 22:12:27 +0530

    make js part

---
+++ js/fashion.js
@@ -0,0 +1,9 @@
+const products = [
+    { id: 1, name: "T-shirt", price: "$20", image: "tshirt.jpg", category: "women" },
+    { id: 2, name: "Jeans", price: "$40", image: "jeans.jpg", category: "clothing" },
+    { id: 3, name: "Jacket", price: "$60", image: "jacket.jpg", category: "women" },
+    { id: 4, name: "Sneakers", price: "$50", image: "footwear" },
+    { id: 5, name: "Sandals", price: "$30", image: "sandals.jpg", category: "women" }
+];
+
+
