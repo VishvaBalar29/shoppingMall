cb3ba5a776b41df853f30ee533cd3c0cccb439d7
Vishva Balar <vishubalar29@gmail.com>
2024-11-22 23:06:31 +0530

    js part done

--- js/fashion.js
+++ js/fashion.js
@@ -1,9 +1,28 @@
-const products = [
-    { id: 1, name: "T-shirt", price: "$20", image: "tshirt.jpg", category: "women" },
-    { id: 2, name: "Jeans", price: "$40", image: "jeans.jpg", category: "clothing" },
-    { id: 3, name: "Jacket", price: "$60", image: "jacket.jpg", category: "women" },
-    { id: 4, name: "Sneakers", price: "$50", image: "footwear" },
-    { id: 5, name: "Sandals", price: "$30", image: "sandals.jpg", category: "women" }
-];
-
-
+const products = {
+    bakery: [
+      { id: 1, name: "Croissant", price: 2.5,subcat:"breads" },
+      { id: 2, name: "Sourdough Bread", price: 4.0,subcat:"breads"  },
+      { id: 3, name: "Sourdough Bread", price: 4.0,subcat:"breads"  },
+      { id: 4, name: "Sourdough Bread", price: 4.0,subcat:"breads"  },
+    ],
+    fruitsVegetables: [
+      { id: 1, name: "Croissant", price: 2.5 , subcat:"fruits" },
+      { id: 2, name: "Sourdough Bread", price: 4.0,subcat:"fruits"  },
+      { id: 3, name: "Sourdough Bread", price: 4.0,subcat:"vegetables"  },
+    ],
+    beauty: [
+      { id: 1, name: "Lipstick", price: 15.0,subcat:"makeup"  },
+      { id: 2, name: "Moisturizer", price: 10.0,subcat:"haircare"  },
+      { id: 3, name: "Moisturizer", price: 10.0,subcat:"skincare"  },
+    ],
+    fashion: [
+      { id: 1, name: "T-shirt", price: 12.0,subcat:"women"  },
+      { id: 2, name: "Jeans", price: 40.0,subcat:"women"  },
+      { id: 3, name: "Jeans", price: 40.0,subcat:"men"  }
+    ],
+    decor: [
+      { id: 1, name: "T-shirt", price: 12.0 ,subcat:"vases" },
+      { id: 2, name: "Jeans", price: 40.0,subcat:"mirrors"  },
+    ],
+};
+  
